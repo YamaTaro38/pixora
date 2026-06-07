d6787d28b2fadcf3bc959fc8236601877512fc2c
YamaTaro38 <yamadotaro38@gmail.com>
2026-06-08 00:23:45 +0700

    rm: mobile dir

--- pixora_mobile/android/app/src/main/kotlin/com/pixora/pixora_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pixora.pixora_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- pixora_mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-// This is a basic Flutter widget test.
-import 'package:flutter_test/flutter_test.dart';
-import 'package:pixora_mobile/main.dart';
-
-void main() {
-  testWidgets('App starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const PixoraApp());
-  });
-}
